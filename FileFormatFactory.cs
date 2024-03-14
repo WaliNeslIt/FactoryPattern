ff4314c50e3a940f6ffc6fd5c599db3900acd4b5
waleed <waleedzia.neslit@gmail.com>
2024-03-15 01:58:44 +0500

    Factory pattern 3-15-2024

---
+++ FileFormatFactory.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPattern
+{
+    public static class FileFormatFactory
+    {
+        public static IFileFormat getFileFormat(string requiredType)
+        {
+            IFileFormat fileFormat = null;
+
+            switch (requiredType)
+            {
+                case "LargeFile":
+                    fileFormat = new LargeFileFormat();
+                break;
+                case "SmallFile":
+                    fileFormat = new SmallFileFormat();
+                break;
+                default:
+                    fileFormat = new DefaultFileExtension();
+                break;
+            }
+            return fileFormat;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using FactoryPattern;
+
+   IFileFormat _fileFormat = FileFormatFactory.getFileFormat("dfkgjdfj");
+   Console.WriteLine("Factory Produced : {0}",_fileFormat.getExtenstion());
+
+
+Console.ReadLine();
